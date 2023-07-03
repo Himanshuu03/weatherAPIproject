37912cb9ccf00d04259ee3d0382610b63d7be10e
Himanshuu03 <himanshujangra2403@gmail.com>
2023-07-03 19:37:47 +0530

    fetching api

---
+++ script.js
@@ -0,0 +1,10 @@
+const API_KEY = "d1845658f92b31c64bd94f06f7188c9c";
+async function fetchWeather(){
+    let city = "haryana";
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`);
+    const data = await response.json();
+    console.log("Weather data:-> " , data);
+    let newPara = document.createElement('p');
+    newPara.textContent = `${data?.main?.temp.toFixed(2)} C`
+    document.body.appendChild(newPara);
+}
